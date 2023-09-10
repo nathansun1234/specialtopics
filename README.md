83e9b4a135f6e1894f1aa9a0aec4a5fb33058bcf
Nathan <nathansun1234@gmail.com>
2023-09-09 19:06:28 -0700

    Finished Hello World Assignment

--- README.md
+++
@@ -1,1 +0,0 @@
-# specialtopics
--- RandomWord.java
+++ RandomWord.java
@@ -1,12 +1,18 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomWord {
     public static void main(String[] args) {
-        String champion = "";
-        for (int i = 1; i <= args.length; i++) {
-            if (Math.random() <= 1/i) {
-                champion = args[i-1];
+        String currentHero = "";
+        int index = 1;
+        while (!StdIn.isEmpty()) {
+            String currentString = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / index)) {
+                currentHero = currentString;
             }
+            index++;
         }
-        System.out.println(champion);
+        StdOut.println(currentHero);
     }
 }
-
